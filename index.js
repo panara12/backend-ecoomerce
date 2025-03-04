ffb2b3b09489df1929abb52ae00f09dfa0122652
panara12 <panaraabhay2@gmail.com>
2025-03-04 23:20:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true,
+    credentials: true, // Needed for authentication (cookies, sessions)
   })
 );
 
